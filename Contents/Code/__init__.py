ba2dd94a6e8cab4f171874250ac62b248e711935
Raymond <marxspawn@gmail.com>
2019-03-30 11:34:10 -0400

    Displays all shows with thumbs
    The Plex web browser portal will display the correct thumbnails and shows. The Android, Windows, and OSX applications will open in icon view but display empty thumbs for each show. All versions will display the correct number of shows and each show link will display the season list/all videos but will not advance to the episode list.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -2,7 +2,7 @@ CW_SEED = 'http://www.cwseed.com'
 CW_SEED_SHOWS = 'http://www.cwseed.com/shows'
 CW_ROOT = 'http://www.cwtv.com'
 PREFIX = '/video/thecwseed'
-NAME = 'The CW SEED'
+NAME = 'CW SEED'
 
 ICON = 'icon-default.jpg'
 RE_JSON = Regex('CWSEED.Site.video_data.videos = (.+);\n')
@@ -16,8 +16,9 @@ def Start():
 ####################################################################################################
 @handler(PREFIX, NAME)
 def MainMenu():
-    
+
     oc = ObjectContainer()
+
     SHOWS_LIST = CW_SEED_SHOWS+'/genre/shows-a-z'
     html = HTML.ElementFromURL(SHOWS_LIST)
     item_list = html.xpath('//ul[@class="list videos-4up"]/li/a')
@@ -28,46 +29,48 @@ def MainMenu():
         thumb = item.xpath('.//div/img/@data-src')[0].split('//')[-1]
         oc.add(DirectoryObject(
             key=Callback(SeedSeasons, url=show_url, title=title),
-            title=title,
-            thumb=thumb
+            title=title, thumb=thumb
         ))
-    # Since this channel contains mostly old shows that will not change, 
+    # Since this channel contains mostly old shows that will not change,
     # we offer the option of reversing the episode order for continuous play from Preferences
     oc.add(PrefsObject(title="Preferences", summary="Set Episode Order"))
     if len(oc) < 2:
         return ObjectContainer(header='Empty', message='There are currently no seasons for this show')
     else:
         return oc
-    
+
 ####################################################################################################
-# Return seasons if listed by seasons or all videos 
 @route(PREFIX + '/seedseasons')
 def SeedSeasons(url, title, thumb=''):
 
     oc = ObjectContainer(title2=title)
-    html = HTML.ElementFromURL(url)
+    seasons_url = CW_SEED + url
+    html = HTML.ElementFromURL(seasons_url)
     if not thumb:
         thumb = html.xpath('//meta[@id="ogimage"]/@content')[0]
     multi_seasons = html.xpath('//div[contains(@id, "seasons-menu2")]/ul/li/a')
 
     if multi_seasons:
         for item in multi_seasons:
-            url = CW_SEED + item.xpath('./@href')[0]
+            url = seasons_url + item.xpath('./@href')[0]
             seas_title = item.xpath('.//text()')[0]
             season = int(url.split('?season=')[1].strip())
             oc.add(DirectoryObject(
-                key = Callback(SeedJSON, url=url, title=seas_title, show_title=title, season=season),
-                title = seas_title,
-                thumb = Resource.ContentsOfURLWithFallback(url=thumb)
+                key=Callback(SeedJSON, url=url, title=seas_title, show_title=title, season=season),
+                title=seas_title, thumb=Resource.ContentsOfURLWithFallback(url=thumb)
             ))
     else:
-        oc.add(DirectoryObject(key = Callback(SeedJSON, url=url, title="All Videos", show_title=title, season=0), title = "All Videos"))
+        oc.add(DirectoryObject(
+            key=Callback(SeedJSON, url=url, title="All Videos",
+            show_title=title, season=0),
+            title="All Videos"
+        ))
 
     if len(oc) < 1:
         return ObjectContainer(header='Empty', message='There are currently no seasons for this show')
     else:
         return oc
-    
+
 ####################################################################################################
 # Pull videos from the json data in the seed formatted video pages
 @route(PREFIX + '/seedjson', season=int)
@@ -84,19 +87,24 @@ def SeedJSON(url, title, season, show_title):
 
     for video in json:
         video_url = CW_ROOT + json[video]['url']
-        try: duration = int(json[video]['dm'].replace('min', ''))
-        except: duration = 0
+        try:
+            duration = int(json[video]['dm'].replace('min', ''))
+        except:
+            duration = 0
         # The guid number is used to pull images from the html
-        try: video_thumb = html.xpath('//li[@data-videoguid="%s"]//img/@data-src' %video)[0]
-        except: video_thumb = None
-        episode = json[video]['en'].replace('Ep.', '').strip()
+        try:
+            video_thumb = html.xpath(
+                '//li[@data-videoguid="%s"]//img/@data-src' % video)[0]
+        except:
+            video_thumb = None
+        episode = json[video]['en'].replace('.', '').strip()
         show = json[video]['st'].strip()
         if episode.isdigit():
-            if len(str(season))>1:
+            if len(str(season)) > 1:
                 season_num = int(episode[0] + episode[1])
             else:
                 season_num = int(episode[0])
-            episode=int(episode)
+            episode = int(episode)
         else:
             season_num = 0
             episode = 0
@@ -105,23 +113,24 @@ def SeedJSON(url, title, season, show_title):
             continue
 
         oc.add(EpisodeObject(
-            show = show,
-            season = season_num,
-            index = episode,
-            duration = duration * 60000,
-            url = video_url,
-            title = json[video]['eptitle'],
-            summary = json[video]['d'],
-            thumb = Resource.ContentsOfURLWithFallback(url=video_thumb)
+            show=show,
+            season=season_num,
+            index=en,
+            duration=duration * 60000,
+            url=video_url,
+            title=json[video]['eptitle'],
+            summary=json[video]['d'],
+            thumb=Resource.ContentsOfURLWithFallback(url=video_thumb)
         ))
-        
+
     # For some reason the json is being sorted out of order so we have to sort it here
-    # Prefs do not work currently in latest apps 
-    sort_order=Prefs['sort_order'] if Prefs['sort_order'] in (True, False) else True
-    oc.objects.sort(key = lambda obj: obj.index, reverse=sort_order)
-        
+    # Prefs do not work currently in latest apps
+    sort_order = Prefs['sort_order'] if Prefs['sort_order'] in (
+        True, False) else True
+    oc.objects.sort(key=lambda obj: obj.index, reverse=sort_order)
+
     if len(oc) < 1:
-        Log ('still no value for objects')
+        Log('still no value for objects')
         return ObjectContainer(header="Empty", message="There are no videos to list.")
     else:
         return oc
